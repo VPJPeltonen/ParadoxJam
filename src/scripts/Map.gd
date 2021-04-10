367707c6d85431b9d0ff39e0a16ff41d65ebe5ba
VPJP <vpjpeltonen@gmail.com>
2021-04-10 15:48:04 +0300

    bigger randomly generated map

--- src/scripts/Map.gd
+++ src/scripts/Map.gd
@@ -1,13 +1,21 @@
 extends Spatial
 
 export (PackedScene) var Tile
+export (PackedScene) var Tile2
+export (PackedScene) var Tile3
 
 var generated = false
+
+onready var rng = RandomNumberGenerator.new()
+onready var tiles = [Tile,Tile2,Tile3]
+
+func _ready():
+	rng.randomize()
 
 func generate():
 	var positions = $Positions.get_children()
 	for pos in positions:
-		var clone = Tile.instance()
+		var clone = tiles[rng.randi_range(0,tiles.size()-1)].instance()
 		$Tiles.add_child(clone)
 		clone.global_transform.origin = pos.global_transform.origin
 		
